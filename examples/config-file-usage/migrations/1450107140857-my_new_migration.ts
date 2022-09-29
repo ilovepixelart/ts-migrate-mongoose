ac576be5a266b2adc1818a606a8f395b71e77731
Alex Eagle <theherocraft@gmail.com>
2022-09-29 13:46:16 +0200

    Cli, naming

--- examples/config-file-usage/migrations/1450107140857-my_new_migration.ts
+++ examples/config-file-usage/migrations/1450107140857-my_new_migration.ts
@@ -1,9 +1,11 @@
+import mongoose from 'mongoose'
 import User from '../models/User'
 
 /**
  * Make any changes you need to make to the database here
  */
 export async function up () {
+  await this.connect(mongoose)
   // Write migration here
   await User.create({ firstName: 'Ada' })
 }
@@ -12,6 +14,7 @@ export async function up () {
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 export async function down () {
+  await this.connect(mongoose)
   // Write migration here
   await User.deleteOne({ firstName: 'Ada' })
 }
