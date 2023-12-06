a55c5ba286b73f971489593ce271cf55115567ee
Alex Eagle <theherocraft@gmail.com>
2023-12-06 09:31:39 +0100

    Delimiter style for interfaces

--- src/interfaces/IMigratorOptions.ts
+++ src/interfaces/IMigratorOptions.ts
@@ -1,7 +1,7 @@
 import type { ConnectOptions } from 'mongoose'
 
 interface IMigratorOptions {
-  uri: string,
+  uri: string
   connectOptions?: ConnectOptions
   templatePath?: string
   migrationsPath?: string
