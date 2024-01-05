3973897653d0313902f549050431fe848a142884
Alex Eagle <theherocraft@gmail.com>
2024-01-05 16:11:50 +0100

    ESM problem with export

--- bash/esm.sh
+++ bash/esm.sh
@@ -6,9 +6,9 @@ root_directory="dist/esm"
 echo "Adding .js extension to relative imports in files:"
 # Find all JavaScript files and iterate through them and add the .js extension to relative imports
 find "$root_directory" -type f -name "*.js" | while read -r file; do
-    if awk '!/export {};|\/\/# sourceMappingURL:|^[[:space:]]*\/\//' "$file" | grep -q "import .* from '[.]\{0,2\}\/[^']*'"; then
-        sed -i.bak -E "s/(import .* from '[.]{0,2}\/[^']*)'/\1.js'/g" "$file" && echo "- $file"
-    fi
+    sed -i.bak -E "s/(import .* from '[.]{0,2}\/[^']*)'/\1.js'/g" "$file"
+    sed -i.bak -E "s/(import \* as .* from '[.]{0,2}\/[^']*)'/\1.js'/g" "$file"
+    sed -i.bak -E "s/(export \* as .* from '[.]{0,2}\/[^']*)'/\1.js'/g" "$file"
 done
 
 echo -e "\nCreating package.json file in $root_directory"
