5de8e95d630a206666782bd0d938871d125db941
Alex <theherocraft@gmail.com>
2025-02-22 18:13:59 +0100

    Biome

--- src/commander.ts
+++ src/commander.ts
@@ -19,11 +19,11 @@ export const getConfig = async (configPath: string): Promise<ConfigOptions> => {
     try {
       const file = path.resolve(configPath)
       const module = (await import(file)) as ConfigOptions | ConfigOptionsDefault | { default?: ConfigOptionsDefault }
-      let fileOptions: ConfigOptions | undefined;
+      let fileOptions: ConfigOptions | undefined
       if ('default' in module) {
-        fileOptions = 'default' in module.default ? module.default.default : module.default as ConfigOptions;
+        fileOptions = 'default' in module.default ? module.default.default : (module.default as ConfigOptions)
       } else {
-        fileOptions = module as ConfigOptions;
+        fileOptions = module as ConfigOptions
       }
 
       if (fileOptions) {
